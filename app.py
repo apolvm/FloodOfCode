389fc91e72dc9f070f4fb026f81485c2b8bb086d
OrangeCoding2021 <55760457+OrangeCoding2021@users.noreply.github.com>
2021-11-08 17:40:20 -0500

    More comments added

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+# Team FloodOfCode's code for HPC in the City: St. Louis 2021 submission
+# Program completed by:
+# Dash App Code - AnaPatricia Olvera
+# Map Display Code - Charles Gaffney
+# Design Outline - Tyler Jackson
+
+# Google Cloud and Deploying Debugging - Cole McKnight and Dr. William Mobley
+# Map Display Coding Guidance and Help - Dr. William Mobley
+
+
+# Import the modules needed 
 from dash.dependencies import Output, Input, State
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
@@ -17,7 +28,7 @@ import datetime
 import json
 import shapely
 import numpy as np
-
+# Create the Server and App
 server = Flask(__name__)
 app = dash.Dash(server=server, external_stylesheets=[dbc.themes.FLATLY])
 app.title = 'Dashboard'
@@ -26,12 +37,15 @@ app.title = 'Dashboard'
 display_type = "open-street-map"
 
 
-
+# Define the app's layout
 app.layout = dbc.Container([ 
-
+    # Create a heading for the app
     dbc.Row(dbc.Col(html.H2("Houston Flooding Map"), width={'size': 12, 'offset': 0, 'order': 0}), 
     style = {'textAlign': 'center', 'paddingBottom': '1%'}),
-
+    # Create the Graph and Slider
+    # Slider max value set to 12 and min value set to 1 to fit 12 months in a year.
+    # Values could be reordered to fit the order a dataset requires
+    # The months start with July due to the dataset we use only covering Jul 2020 to Jun 2021
     dbc.Row(dbc.Col(dcc.Loading(children=[dcc.Graph(id='Houston'),
                                           dcc.Slider(id='my_slider',min=1, max=12,value=8,
                                           marks={
@@ -50,17 +64,21 @@ app.layout = dbc.Container([
                                               })
                                         ], color = '#000000', type = 'dot', fullscreen=True ) ))
 ])
-
+# Process the app's output and input.
 @app.callback(
     Output(component_id='Houston', component_property='figure'),
     Input(component_id='my_slider', component_property='value')
 )
-
+# Runs code to display the figure (map). A integer is passed as the month to sort what flood events are displayed
 def update_figure(month):
+    # Declare a variable for month that does not try changing the received input
     month_internal = 0
+    # Due to the data the dataset contains, for the first 6 month value possibilities.
+    # Set month_internal to 6 added to the month value from the slider and set the year to 2020
     if month in [1,2,3,4,5,6]:
         month_internal = month+6
         year = 2020
+    # For the other month values, subtract 6 and set the year to 2021
     else: # month == 7 or 8 or 9 or 10 or 11 or 12 :
         month_internal = month-6
         year = 2021
@@ -73,7 +91,7 @@ def update_figure(month):
 
     # Get the heatmap data
     heatmap = pandas.read_csv('floodingheatmap12m.csv', sep='|')
-
+    # Make sure values in 'Create Date' column are in datetime
     heatmap['Create Date'] = pandas.to_datetime(heatmap['Create Date'])
     
 
@@ -82,7 +100,7 @@ def update_figure(month):
     #print(str(month) + "to" + str(month_internal))
 
 
-    # Get the indexes of the flood events occuring in the selected month.
+    
 
 
 
@@ -95,14 +113,15 @@ def update_figure(month):
     # Get it to right format
     heatmap_gdf = heatmap_gdf.set_crs('epsg:4326')
 
-#    Get another copy of shapes (roads) before messing with it
+#    Get another copy of shapes (roads) before using it
     before_shapes = shapes.copy(deep=True)
 
-        # Create a buffer on shapes.geometry
+    # Create a buffer on shapes.geometry
     shapes.geometry = shapes.buffer(.0002)
+    # Add month and year columns to the GeoDataFrame
     heatmap_gdf['DFmonth']=heatmap_gdf.apply(lambda row: row.createDate.month, axis=1)
     heatmap_gdf['DFyear']=heatmap_gdf.apply(lambda row: row.createDate.year, axis=1)
-    # Spatial join the roads gdf and the heatmap gdf 
+    # Spatial join the roads gdf with the points from the heatmap that are specifically from the requested month and year
     flooded_roads = gpd.sjoin(shapes,heatmap_gdf.loc[(heatmap_gdf.DFmonth== month_internal) & (heatmap_gdf.DFyear== year)],
                               'inner',predicate='contains')
     #flooded_roads = gpd.sjoin(shapes,heatmap_gdf,'inner',predicate='contains')
@@ -114,8 +133,8 @@ def update_figure(month):
     # Future proofing if we find easy way to add sattelite image
     display_type = "open-street-map"
 
-    # CREATE FIGURE AS SCATTER MAPBOX OF THE HEATMAP
-    # https://plotly.com/python/lines-on-mapbox/
+    # Create the Scattermapbox of all the flood event points from the requested time period found in heatmap_gdf
+    
     fig=go.Figure(go.Scattermapbox(
                     lat=heatmap.loc[(heatmap_gdf.DFmonth== month_internal) & (heatmap_gdf.DFyear== year)].lat,
                     lon=heatmap.loc[(heatmap_gdf.DFmonth== month_internal) & (heatmap_gdf.DFyear== year)].lon,
@@ -127,21 +146,25 @@ def update_figure(month):
                         bgcolor='darkslateblue',
                         bordercolor='lightgrey'
                     )))
-
+    # Draw each flooded road
     for i in fr_index: #Change back to list(fr_index)
+        # Get the shape and name of each road
         feature= before_shapes.iloc[i].geometry
         name = before_shapes.iloc[i].FULLNAME
         #print(f"{feature} name {name}")
-        # Prints poly gon and name
+        # Create needed empty lists
         lats = []
         lons = []
         names = []
+        
+        # Check to see what type of geometry each shape is and store data based on the type
         if isinstance(feature, shapely.geometry.linestring.LineString):
             linestrings = [feature]
         elif isinstance(feature, shapely.geometry.multilinestring.MultiLineString):
             linestrings = feature.geoms
         else:
             continue
+        # For each linestring, grab everything that is needed to add the trace of it.
         for linestring in linestrings:
             x, y = linestring.xy
             lats = np.append(lats, y)
@@ -150,13 +173,13 @@ def update_figure(month):
             lats = np.append(lats, None)
             lons = np.append(lons, None)
             names = np.append(names, None)
-        # https://plotly.github.io/plotly.py-docs/generated/plotly.graph_objects.Scattermapbox.html
-        #print(name)
         
+        #print(name)
+        # Add the flooded roads to the map display
         fig.add_trace(go.Scattermapbox(mode='lines',lat=lats, lon=lons, name=name,line=go.scattermapbox.Line(
         color = 'aqua',
         width = 5)))
-
+    # Final updates to the display
     fig.update_layout(
         # mapbox_style=display_type,
         margin={"r": 0, "t": 0, "l": 0, "b": 0},
@@ -168,8 +191,8 @@ def update_figure(month):
             'center': {'lat': 29.749907, 'lon': -95.358421},
             'zoom': 10}
     )
-
+    # Return the figure
     return fig
-
+# Only run the program if this file in particular is being run
 if __name__=='__main__':
     app.run_server(debug=True)
